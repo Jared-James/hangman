68bb7ea44c0aa2a2a6d921743c4f60f0477c4476
jared james <jared_work@outlook.com>
2020-07-10 20:09:35 +1200

    needed to change the https:// request to ://

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async () => {
-    const response = await fetch('http://puzzle.mead.io/puzzle')
+    const response = await fetch('//puzzle.mead.io/puzzle')
     if (response.status === 200) {
         const data = await response.json()
         return data.puzzle
@@ -11,7 +11,7 @@ const getPuzzle = async () => {
 
 
 const getCountry = async (code) => {
-    const response = await fetch('https://restcountries.eu/rest/v2/all')
+    const response = await fetch('//restcountries.eu/rest/v2/all')
     if (response.status === 200) {
         const data = await response.json()
         const myCountry = data.find(country => country.alpha2Code === code)
@@ -22,7 +22,7 @@ const getCountry = async (code) => {
 }
 
 const getLocation = async () => {
-  const data = await fetch('http://ipinfo.io/json?token=6f86111e4ab6a2')
+  const data = await fetch('//ipinfo.io/json?token=6f86111e4ab6a2')
   if (data.status === 200) {
       const response = await data.json()
       return response.country
